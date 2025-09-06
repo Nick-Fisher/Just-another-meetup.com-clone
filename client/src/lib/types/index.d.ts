9f6cba8524f9e33bdf4f3db10e3baa7898f1fdb5
Nick Fisher <sincerelyyoursnikita@gmail.com>
2025-09-07 00:06:01 +0200

    Basic frontend

--- client/src/lib/types/index.d.ts
+++ client/src/lib/types/index.d.ts
@@ -1,12 +1,12 @@
-type Meeting = {
-  id: string;
-  title: string;
-  date: string;
-  description: string;
-  category: string;
-  isCancelled: boolean;
-  city: string;
-  venue: string;
-  latitude: number;
-  longitude: number;
-};
+type Activity = {
+    id: string
+    title: string
+    date: string
+    description: string
+    category: string
+    isCancelled: boolean
+    city: string
+    venue: string
+    latitude: number
+    longitude: number
+}
